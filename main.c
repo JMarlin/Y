ab6d68d9f7754a17d6f0c792beff41aacee2e14a
Joseph Marlin <j.marlin@outlook.com>
2022-02-28 08:28:55 -0500

    Basic symbol parse first pass done, starting expression parsing

--- main.c
+++ main.c
@@ -1,28 +1,45 @@
 #include <inttypes.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef enum {
-    Module;
-    Statement;
-    Expression;
-    SymbolReference;
-    Lambda;
-    LiteralValue;
-    Operator;
+    Module,
+    Statement,
+    Expression,
+    SymbolReference,
+    Lambda,
+    LiteralValue,
+    Operator 
 } ASTNodeType;
 
 typedef enum {
-    SymbolDeclaration;
+    SymbolDeclaration
 } ASTStatementType;
 
 typedef enum {
-    Assignment;
-    Addition;
+    Assignment,
+    Addition
 } ASTExpressionType;
+
+typedef struct String_s {
+    uint32_t capacity;
+    uint32_t length;
+    char* data;
+} String;
 
 typedef struct ASTNode_s {
     ASTNodeType type;
 } ASTNode;
+
+typedef struct ASTStatementNode_s {
+    ASTNodeType type;
+    ASTStatementType statementType;
+} ASTStatementNode;
+
+typedef struct ASTLiteralType_s {
+    
+} ASTLiteralType;
 
 typedef struct ASTModuleNode_s {
     ASTNodeType type;
@@ -30,18 +47,13 @@ typedef struct ASTModuleNode_s {
     ASTStatementNode* statement;
 } ASTModuleNode;
 
-typedef struct ASTStatementNode_s {
-    ASTNodeType type;
-    ASTStatementType statementType;
-} ASTStatementNode;
-
 typedef struct ASTExpressionNode_s {
     ASTNodeType type;
     ASTExpressionType expressionType;
 } ASTExpressionNode;
 
 typedef struct ASTSymbol_s {
-    char* text;
+    String* text;
 } ASTSymbol;
 
 typedef struct ASTAssignmentExpression_s {
@@ -64,12 +76,12 @@ typedef struct ASTLiteralExpression_s {
     ASTLiteralType literalType;
 } ASTLiteralExpression;
 
-typedef struct ASTUnt32LiteralExpression_s {
+typedef struct ASTUInt32LiteralExpression_s {
     ASTNodeType type;
     ASTExpressionType expressionType;
     ASTLiteralType literalType;
     uint32_t value;
-}
+} ASTUInt32LiteralExpression;
 
 void skip_whitespace(FILE* in_file) {
     
@@ -89,6 +101,134 @@ void skip_whitespace(FILE* in_file) {
             return;
         }
     }
+}
+
+void String_init(String* string, char* s) {
+
+    string->length = s == 0 ? 0 : strlen(s);
+    string->capacity = 0;
+    string->data = s;
+}
+
+String* String_new(char* s) {
+
+    String* string = (String*)malloc(sizeof(String));
+
+    if(!string) return string;
+
+    String_init(string, s);
+
+    return string;
+}
+
+char* String_append(String* target, String* source) {
+
+    int reallocate = 0;
+    
+    //TODO: There has to be a fast math way to calculate the
+    //      next highest power of 2 from an arbitrary number
+    while((target->length + source->length) > target->capacity) {
+
+        target->capacity = target->capacity == 0
+            ? 1
+            : (2 * target->capacity);
+    }
+
+    if(reallocate) {
+
+        target->data = (char*)realloc(target->data, target->capacity);
+
+        if(!target->data) return "Failed to reallocate string buffer";
+    }
+
+    //TODO: error handle
+    strncpy(&target->data[target->length], source->data, source->length);
+
+    target->length = target->length + source->length;
+
+    return 0;
+}
+
+char* String_appendChar(String* string, char c) {
+
+    String temp;
+
+    temp.capacity = 0;
+    temp.length = 1;
+    temp.data = &c;
+
+    String_append(string, &temp);
+}
+
+void String_cleanUp(String* string) {
+
+    if(string->capacity > 0) free(string->data);
+
+    free(string);
+}
+
+void ASTSymbol_cleanUp(ASTSymbol* symbol) {
+
+    String_cleanUp(symbol->text);
+
+    free(symbol);
+}
+
+char* ASTSymbol_tryParse(FILE* in_file, ASTSymbol** symbol) {
+
+    fpos_t original_position;
+    char c = 0;
+    String* symbol_text = String_new(0);
+
+    if(!symbol_text) return "Unable to allocate String for symbol text";
+
+    if(fgetpos(in_file, &original_position) != 0) return "Failed to get file position";
+
+    skip_whitespace(in_file);
+
+    while(1) {
+
+        if(fread(&c, 1, 1, in_file) != 1) break;
+
+        if(
+            (c >= 'a' && c <= 'z') ||
+            (c >= 'A' && c <= 'Z') ||
+            (i > 0 && c >= '0' && c <= '9') ||
+            (c == '_')
+        ) {
+
+            char* error = String_appendChar(symbol_text, c);
+
+            if(error != 0) {
+        
+                String_cleanUp(symbol_text);
+
+                return error;
+            }
+
+            continue;
+        }
+
+        break;
+    }
+
+    (*symbol) = (ASTSymbol*)malloc(sizeof(ASTSymbol));
+
+    if(!(*symbol)) {
+
+        String_cleanUp(symbol_text);
+
+        return "Couldn't allocate memory for ast symbol";
+    }
+
+    (*symbol)->text = symbol_text;
+
+    return 0;
+}
+
+char* ASTExpressionNode_tryParse(FILE* in_file, ASTExpressionNode** expression_node) {
+
+    //TODO: Here
 }
 
 char* ASTDeclarationStatement_tryParse(FILE* in_file, ASTStatementNode** statement_node) {
